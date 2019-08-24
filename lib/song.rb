5f59949b90840b884c27f0023134abd4a3396b60
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-24 18:02:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
     end
     
     def self.find_or_create_by_name(name)
-      self.find_by_name{|x|
+      self.find_by_name(name) || self.
     
   def save
     self.class.all << self
