f918dd45c00eb84b5d355a4a4bb19192c0244fd0
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-24 17:25:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,3 @@
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
   
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-  
-  def self.create
-    @@all << create
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def save
-    self.class.all << self
-  end
 end
