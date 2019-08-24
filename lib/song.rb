076e609cda84c39c1acbdf31d5170bbef055c5eb
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-24 18:12:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,11 +40,9 @@ class Song
     
     def self.new_from_filename
       song = self.new 
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
-    song
+    song.name = split
   end
-  song
+  
   
   def save
     self.class.all << self
